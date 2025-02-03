c763424e62a207db82d646e502293fd820db4232
capel201 <cam.pell@me.com>
2025-02-03 12:52:42 -0500

    reorganisation

--- Simple Shiny.R
+++
@@ -1,8 +0,0 @@
-# required to read JSON files.
-library("rjson")
-# Passing argument files
-myData <- fromJSON(list.files,(path = '_SharedFolder_GlobalES/Dashboard/Canada/json_files'))
-# Convert JSON file to dataframe.
-json_data_frame <- as.data.frame(myData)
-print(json_data_frame)
-
